40995a9abf7c389ffb3fe037b37f7a03f9457eb6
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2024-10-16 01:14:07 +0530

    Time: 8 ms (27.96%), Space: 11.7 MB (34.6%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        vector<int> arr(triangle[0].size());
-        for(int i=0; i<triangle.size(); i++)
+        vector<int> arr(triangle[triangle.size()-1].size());
+        for(int i=triangle.size()-1; i>=0; i--)
         {
-            vector<int> temp(triangle[i].size());
-            for(int j=0; j<triangle[i].size(); j++)
+            vector<int> temp(triangle[triangle.size()-1].size());
+            for(int j=triangle[i].size()-1; j>=0; j--)
             {
-                int up = INT_MAX;
-                int upleft = INT_MAX;
-                if(i>0 && j<triangle[i].size()-1) up = arr[j];
-                if(j>0) upleft = arr[j-1];
-                if(up == INT_MAX && upleft == INT_MAX) {temp[j] = triangle[i][j]; cout << temp[j] << " "; continue;}
-                temp[j] = triangle[i][j] + min(up, upleft);
-                cout << temp[j] << " ";
+                int down = INT_MAX;
+                int downright = INT_MAX;
+                if(j<triangle.size()-1)
+                {
+                    down = arr[j];
+                    downright = arr[j+1];
+                }
+                if(down == INT_MAX) {temp[j] = triangle[i][j]; continue;}
+                else temp[j] = triangle[i][j] + min(down, downright);
             }
-            cout << endl;
             arr = temp;
         }
-        return *min_element(arr.begin(), arr.end());
+        return arr[0] ;
     }
 };
