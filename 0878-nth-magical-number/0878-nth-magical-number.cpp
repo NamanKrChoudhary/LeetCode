c3dba77a1a3c9a9e01dcf007e8e42b3d4f5625c1
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2024-12-18 19:03:40 +0530

    Time: 0 ms (100%), Space: 8 MB (23.04%) - LeetHub

--- 0878-nth-magical-number/0878-nth-magical-number.cpp
+++ 0878-nth-magical-number/0878-nth-magical-number.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     int nthMagicalNumber(int n, int a, int b) {
-        long long int one = min(a,b);
-        long long int two = max(a,b);
-        long long int pp = 0;
-        while(one*pp<=two)
+        long long int lcm = b*(a/gcd(a,b));
+        long long int s=0, e=1e14, mid = s+((e-s)/2);
+        long long int dd = 1e9 + 7;
+        long long int ans = 0;
+        while(s<=e)
         {
-            if(one*(pp+1) > two) break;
-            pp = pp+1;
+            mid = s+((e-s)/2);
+            long long int temp = (mid/a)+(mid/b)-(mid/lcm);
+            if(temp >= n) {ans = mid%(dd); e = mid-1;}
+            else s = mid+1;
         }
-        pp = pp+1;
-        long long int i = 0;
-        long long int curr = 0;
-        for(; i<=n; i++)
-        {
-            if((i*((2*pp)+i-1)/2) > n) {--i; break;}
-        }
-        curr = n - (i*((2*pp)+i-1)/2);
-        pp = pp-1;
-        if(curr <= pp) {return pow(one, (pp*i)) * pow(one,curr) ;}
-        else {return pow(two,(curr-pp)) * pow(one,(pp*(i+1-(curr-pp))));}
-        return 0;
+        return ans;
     }
 };
