d1eb44b28afadf91e03adabbb8a7205b1a419e3e
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2025-04-06 17:33:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    void dfs(vector<vector<bool>>& vis, vector<vector<int>>& image, int curri, int currj, int clr, int iclr)
+    void dfs(vector<vector<bool>>& vis, vector<vector<int>>& image, int curri, int currj, int clr)
     {
         vis[curri][currj] = true;
-        if(curri>0 && !vis[curri-1][currj] && image[curri-1][currj] == iclr) dfs(vis, image, curri-1, currj, clr, iclr);
-        if(currj>0 && !vis[curri][currj-1] && image[curri][currj-1] == iclr) dfs(vis, image, curri, currj-1, clr, iclr);
-        if(curri<image.size()-1 && !vis[curri+1][currj] && image[curri+1][currj] == iclr) dfs(vis, image, curri+1, currj, clr, iclr);
-        if(currj<image[0].size()-1 && !vis[curri][currj+1] && image[curri][currj+1] == iclr) dfs(vis, image, curri, currj+1, clr, iclr);
+        if(curri>0 && !vis[curri-1][currj] && image[curri-1][currj] == image[curri][currj]) dfs(vis, image, curri-1, currj, clr);
+        if(currj>0 && !vis[curri][currj-1] && image[curri][currj-1] == image[curri][currj]) dfs(vis, image, curri, currj-1, clr);
+        if(curri<image.size()-1 && !vis[curri+1][currj] && image[curri+1][currj] == image[curri][currj]) dfs(vis, image, curri+1, currj, clr);
+        if(currj<image.size()-1 && !vis[curri][currj+1] && image[curri][currj+1] == image[curri][currj]) dfs(vis, image, curri, currj+1, clr);
         image[curri][currj] = clr;
         return;
     }
@@ -14,8 +14,7 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n= image.size(), m = image[0].size();
         vector<vector<bool>> vis(n, vector<bool>(m));
-        int iclr = image[sr][sc];
-        dfs(vis, image, sr, sc, color, iclr);
+        dfs(vis, image, sr, sc, color);
         return image;
     }
 };
