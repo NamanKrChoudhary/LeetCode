7150d6f5bd979258a6be47a7625f25b57588f876
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2025-05-12 15:45:09 +0530

    Time: 0 ms (100%), Space: 17.6 MB (18.65%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
+    void dfs(vector<bool>& vis, vector<long long int>& val, vector<vector<int>>& adj, long long int curr, vector<bool>& ans)
+    {
+        for(int i=0; i<adj[curr].size(); i++)
+        {
+            if(vis[adj[curr][i]] == false)
+            {
+                vis[adj[curr][i]] = true;
+                val[adj[curr][i]] = !val[curr];
+                dfs(vis, val, adj, adj[curr][i], ans);
+            }
+            else
+            {
+                if(val[curr] == val[adj[curr][i]]) {ans[0] = false; return;}
+            }
+        }
+    }
     bool isBipartite(vector<vector<int>>& graph) {
         vector<bool> vis(graph.size());
         vector<long long int> val(graph.size());
+        vector<bool> ans(1);
+        ans[0] = true;
         for(int i=0; i<graph.size(); i++)
         {
-            if(!vis[i])
-            {
-                queue<pair<long long int, long long int>> q;
-                q.push({0,i});
-                vis[i] = true;
-                val[i] = 0;
-                while(!q.empty())
-                {
-                    pair<long long int, long long int> temp = q.front();
-                    q.pop();
-                    for(int j=0; j<graph[temp.second].size(); j++)
-                    {
-                        if(vis[graph[temp.second][j]] == true && val[graph[temp.second][j]] == temp.first) return false;
-                        else if(vis[graph[temp.second][j]] == false)
-                        {
-                            vis[graph[temp.second][j]] = true;
-                            val[graph[temp.second][j]] = (temp.first+1)%2;
-                            q.push({val[graph[temp.second][j]], graph[temp.second][j]});
-                        }
-                    }
-                }
-            }
+            if(!vis[i] && ans[0] == true) dfs(vis, val, graph, i, ans);
         }
-        return true;
+        return ans[0];
     }
 };
