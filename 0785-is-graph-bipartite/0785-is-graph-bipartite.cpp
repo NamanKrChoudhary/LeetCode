a5c5eec082396e0325417dd085d53f08db5297ed
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2025-05-12 14:37:27 +0530

    Time: 7 ms (7.44%), Space: 18 MB (11.04%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        vector<bool> vis(graph.size());
+        vector<long long int> val(graph.size());
+        bool check = true;
+        for(int i=0; i<graph.size(); i++)
+        {
+            if(!vis[i])
+            {
+                queue<pair<long long int, long long int>> q;
+                q.push({0,i});
+                vis[i] = true;
+                val[i] = 0;
+                while(!q.empty())
+                {
+                    pair<long long int, long long int> temp = q.front();
+                    q.pop();
+                    for(int j=0; j<graph[temp.second].size(); j++)
+                    {
+                        if(vis[graph[temp.second][j]] == true && val[graph[temp.second][j]] == temp.first) return false;
+                        else if(vis[graph[temp.second][j]] == false)
+                        {
+                            vis[graph[temp.second][j]] = true;
+                            val[graph[temp.second][j]] = (temp.first+1)%2;
+                            q.push({val[graph[temp.second][j]], graph[temp.second][j]});
+                        }
+                    }
+                }
+            }
+        }
+        for(int i=0; i<graph.size(); i++) cout << val[i] << " "; cout << endl;
+        return true;
+    }
+};
