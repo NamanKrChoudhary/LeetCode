cbc3dbe08d73724f08b7595e9c3a3e7cdffe4067
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2025-05-18 16:15:49 +0530

    Time: 11 ms (13.42%), Space: 20.6 MB (5.43%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    bool check(vector<bool>& vis, vector<bool>& path, vector<bool>& truth, vector<vector<int>>& adj, long long int curr)
+    bool check(vector<bool>& vis, vector<bool>& path, vector<bool>& truth, vector<vector<int>>& adj, long long int curr, vector<int>& ans)
     {
         vis[curr] = true;
         path[curr] = true;
         bool checker = true;
         for(int i=0; i<adj[curr].size(); i++)
         {
-            if(!vis[adj[curr][i]] && check(vis, path, truth, adj, adj[curr][i]) == false) checker = false;
+            if(!vis[adj[curr][i]] && check(vis, path, truth, adj, adj[curr][i], ans) == false) checker = false;
             else if(vis[adj[curr][i]] && path[adj[curr][i]]) checker = false;
             else if(vis[adj[curr][i]] && truth[adj[curr][i]] == false) checker = false;
         }
         if(checker == false) {path[curr] = false; return false;}
+        ans.push_back(curr);
         path[curr] = false;
         truth[curr] = true; return true;
     }
@@ -22,29 +23,16 @@ public:
         for(int i=0; i<p.size(); i++) {adj[p[i][0]].push_back(p[i][1]); adj1[p[i][1]].push_back(p[i][0]);}
         vector<bool> vis(n,0), path(n,0), truth(n,0);
         bool checker = true;
+        vector<int> temp;
         for(int i=0; i<n; i++)
         {
-            if(!vis[i]) checker = check(vis, path, truth, adj, i);
+            if(!vis[i]) checker = check(vis, path, truth, adj, i, temp);
             if(checker == false) break;
         }
         vector<int> ans;
         if(checker == false) return ans;
-        vector<bool> v(n,0);
-        for(int i=0; i<n; i++)
-        {
-            if(!v[i] && adj1[i].size() == 0)
-            {
-                queue<int> q;
-                q.push(i); v[i] = true;
-                while(!q.empty())
-                {
-                    int temp = q.front();
-                    ans.push_back(temp); q.pop();
-                    for(int j=0; j<adj[temp].size(); j++) if(!v[adj[temp][j]]) {v[adj[temp][j]] = true; q.push(adj[temp][j]);}
-                }
-            }
-        }
-        reverse(ans.begin(), ans.end());
+        ans = temp;
+        //reverse(ans.begin(), ans.end());
         return ans;
     }
 };
