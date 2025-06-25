cc515a95ba2db63f49f0b9c5915ebca55b1993ba
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2025-06-26 01:00:47 +0530

    Time: 1168 ms (5.14%), Space: 102.7 MB (23.87%) - LeetHub

--- 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.cpp
+++ 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.cpp
@@ -1,40 +1,84 @@
 class Solution {
 public:
-    int f(vector<int>& nums2, long long x1, long long v) {
-        int n2 = nums2.size();
-        int left = 0, right = n2 - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (x1 >= 0 && nums2[mid] * x1 <= v ||
-                x1 < 0 && nums2[mid] * x1 > v) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        if (x1 >= 0) {
-            return left;
-        } else {
-            return n2 - left;
-        }
-    }
 
-    long long kthSmallestProduct(vector<int>& nums1, vector<int>& nums2,
-                                 long long k) {
-        int n1 = nums1.size();
-        long long left = -1e10, right = 1e10;
-        while (left <= right) {
-            long long mid = (left + right) / 2;
-            long long count = 0;
-            for (int i = 0; i < n1; i++) {
-                count += f(nums2, nums1[i], mid);
+    const long long int ZZEERROO = 0;
+
+    long long kthSmallestProduct(vector<int>& nums1, vector<int>& nums2, long long k) {
+        long long int least = -1e10, greatest = 1e10, kth = least + (greatest - least)/2;
+        vector<long long int> neg2(nums2.size());
+        for(int i=0; i<nums2.size(); i++) neg2[i] = (-1)*nums2[nums2.size()-1-i];
+        long long int minn = 1LL*nums1[0]*nums2[0];;
+        while(least <= greatest)
+        {
+            kth = least + (greatest - least)/2;
+            long long int cntless = 0;
+            bool have = false;
+            long long int pos = 1e10;
+            for(int i=0; i<nums1.size(); i++)
+            {
+                if(nums1[i] == 0 && kth >= 0) {cntless = cntless + nums2.size(); if(0>=kth)pos = min(pos, ZZEERROO); continue;}
+                else if(nums1[i] == 0) continue;
+                if(kth >= 0)
+                {
+                    if(nums1[i] >= 0)
+                    {
+                        long long int req = kth/nums1[i];
+                        if(kth%nums1[i] != 0) req = req;
+                        long long int cnt = upper_bound(nums2.begin(), nums2.end(), req) - nums2.begin();
+                        long long int have = lower_bound(nums2.begin(), nums2.end(), req) - nums2.begin();
+                        long long int currpos = pos;
+                        if(have < nums2.size()) currpos = (1LL*nums1[i]*nums2[have]);
+                        if(have < nums2.size() && currpos >= kth) pos = min(pos, currpos);
+                        cntless = cntless + cnt;
+                    }
+                    else
+                    {
+                        long long int req = (-1)*(kth/nums1[i]);
+                        if(kth%nums1[i] != 0) req = req ;
+                        long long int cnt = upper_bound(neg2.begin(), neg2.end(), req) - neg2.begin();
+                        long long int have = lower_bound(neg2.begin(), neg2.end(), req) - neg2.begin();
+                        long long int currpos = pos;
+                        if(have < nums2.size()) currpos = (1LL*nums1[i]*neg2[have]*(-1));
+                        if(have < nums2.size() && currpos >= kth)pos = min(pos, currpos);
+                        cntless = cntless + cnt;
+                    }
+                }
+                else
+                {
+                    if(nums1[i] >= 0)
+                    {
+                        long long int req = kth/nums1[i];
+                        if(kth%nums1[i] != 0) req = req - 1;
+                        long long int cnt = upper_bound(nums2.begin(), nums2.end(), req) - nums2.begin();
+                        long long int have = lower_bound(nums2.begin(), nums2.end(), req) - nums2.begin();
+                        long long int currpos = pos;
+                        if(have < nums2.size()) currpos = (1LL*nums1[i]*nums2[have]);
+                        if(have < nums2.size() && currpos >= kth) pos = min(pos, currpos);
+                        cntless = cntless + cnt;
+                    }
+                    else
+                    {
+                        long long int req = (-1)*(kth/nums1[i]);
+                        if(kth%nums1[i] != 0) req = req - 1;
+                        long long int cnt = upper_bound(neg2.begin(), neg2.end(), req) - neg2.begin();
+                        long long int have = lower_bound(neg2.begin(), neg2.end(), req) - neg2.begin();
+                        long long int currpos = pos;
+                        if(have < nums2.size()) currpos = (1LL*nums1[i]*neg2[have]*(-1));
+                        if(have < nums2.size() && currpos >= kth) pos = min(pos,currpos);
+                        cntless = cntless + cnt;
+                    }
+                }
+                //if(kth == 9)cout << i << " " << cntless << endl;
             }
-            if (count < k) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+            //cout << least << " " << greatest << " " << kth << " " << cntless << endl;
+            if(cntless >= k)
+            {
+                //cout << kth << " " << pos << " " << cntless << endl;
+                minn = pos;
+                greatest = kth-1;
             }
+            else least = kth + 1;
         }
-        return left;
+        return minn;
     }
 };
