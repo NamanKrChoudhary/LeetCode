9760c7cbf7034d5d7549f315efd2aa03848379b7
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2025-08-08 02:23:12 +0530

    Time: 83 ms (56.17%), Space: 174.1 MB (25.31%) - LeetHub

--- 3363-find-the-maximum-number-of-fruits-collected/3363-find-the-maximum-number-of-fruits-collected.cpp
+++ 3363-find-the-maximum-number-of-fruits-collected/3363-find-the-maximum-number-of-fruits-collected.cpp
@@ -1,40 +1,58 @@
 class Solution {
 public:
+    // state - dp[i][j] = the maximum amt a child can collect while reaching the n,n and also considering
+    // n-1 moves, both the children wont meet and one child can only move till the diagonal, or below it else it wont be able to reach the n,n in the specified moves
+
+    int maxnumni(vector<vector<int>>& grid, vector<vector<int>>& dp, int curri, int currj)
+    {
+        if(currj > curri) return 0;
+        if(dp[curri][currj] != -1) return dp[curri][currj];
+        if(curri == grid.size()-1 && currj == grid.size()-1) dp[curri][currj] = grid[curri][currj];
+        else if(curri == currj) dp[curri][currj] = grid[curri][currj] + maxnumni(grid, dp, curri+1, currj+1);
+        else
+        {
+            int curr1 = 0, curr2 = 0, curr3 = 0;
+            if(curri < grid.size()-1) curr1 = maxnumni(grid, dp, curri+1, currj+1);
+            curr2 = maxnumni(grid, dp, curri, currj+1);
+            curr3 = maxnumni(grid, dp, curri-1, currj+1);
+            dp[curri][currj] = grid[curri][currj] + max(curr1, max(curr2, curr3));
+        }
+        return dp[curri][currj];
+    }
+
+    int maxnumin(vector<vector<int>>& grid, vector<vector<int>>& dp, int curri, int currj)
+    {
+        if(curri > currj) return 0;
+        if(dp[curri][currj] != -1) return dp[curri][currj];
+        else if(curri == grid.size()-1 && currj == grid.size()-1) return dp[curri][currj] = grid[curri][currj];
+        else if(curri == currj) return dp[curri][currj] = grid[curri][currj] + maxnumin(grid, dp, curri+1, currj+1);
+        else
+        {
+            int curr1 = 0, curr2 = 0, curr3 = 0;
+            if(currj < grid.size()-1) curr1 = maxnumin(grid, dp, curri+1, currj+1);
+            curr2 = maxnumin(grid, dp, curri+1, currj);
+            curr3 = maxnumin(grid, dp, curri+1, currj-1);
+            dp[curri][currj] = grid[curri][currj] + max(curr1, max(curr2, curr3));
+        }
+        return dp[curri][currj];
+    }
+
     int maxCollectedFruits(vector<vector<int>>& fruits) {
         int n = fruits.size();
-        int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            ans += fruits[i][i];
-        }
-
-        auto dp = [&]() -> int {
-            vector<int> prev(n, INT_MIN), curr(n, INT_MIN);
-            prev[n - 1] = fruits[0][n - 1];
-            for (int i = 1; i < n - 1; ++i) {
-                for (int j = max(n - 1 - i, i + 1); j < n; ++j) {
-                    int best = prev[j];
-                    if (j - 1 >= 0) {
-                        best = max(best, prev[j - 1]);
-                    }
-                    if (j + 1 < n) {
-                        best = max(best, prev[j + 1]);
-                    }
-                    curr[j] = best + fruits[i][j];
-                }
-                swap(prev, curr);
-            }
-            return prev[n - 1];
-        };
-
-        ans += dp();
-
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                swap(fruits[j][i], fruits[i][j]);
+        int maxx = 0;
+        vector<vector<int>> grid(n, vector<int>(n));
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(i == j) maxx = maxx + fruits[i][j];
+                if(i != j) grid[i][j] = fruits[i][j];
             }
         }
-
-        ans += dp();
-        return ans;
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        int lchild = maxnumni(grid, dp, n-1, 0);
+        for(int i=0; i<n; i++) for(int j=0; j<n; j++) dp[i][j] = -1;
+        int rchild = maxnumin(grid, dp, 0, n-1);
+        return maxx + lchild + rchild;
     }
 };
