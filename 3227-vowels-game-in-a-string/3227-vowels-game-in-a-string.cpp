815866aa5c05a0f8d5415c073c5b8816664c0cad
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2025-09-12 13:11:48 +0530

    Time: 19 ms (9.89%), Space: 19.5 MB (13.62%) - LeetHub

---
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        set<char> ss = {'a', 'e', 'i', 'o', 'u'};
+        bool check = false;
+        for(int i=0; i<s.size(); i++)
+        {
+            if(ss.find(s[i]) != ss.end()){check = true; break;}
+        }
+        return check;
+    }
+};
