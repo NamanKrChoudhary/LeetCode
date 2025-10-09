977a2b7ababacbbd1dbedb52920d346c61c7853f
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2025-10-09 17:07:39 +0530

    Time: 483 ms (55.05%), Space: 45.6 MB (43.43%) - LeetHub

--- 3494-find-the-minimum-amount-of-time-to-brew-potions/3494-find-the-minimum-amount-of-time-to-brew-potions.cpp
+++ 3494-find-the-minimum-amount-of-time-to-brew-potions/3494-find-the-minimum-amount-of-time-to-brew-potions.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
+    using ll = long long;
     long long minTime(vector<int>& skill, vector<int>& mana) {
-        long long int n = skill.size(), m = mana.size();
-        vector<long long int> currend(n,0);
-        long long int st = 0;
-        long long int curr = 0;
-        for(int j=0; j<m; j++)
-        {
-            long long int curradd = 0;
-            curr = 0;
-            for(int i=0; i<n; i++)
-            {
-                long long int prevend = curr + curradd;
-                if(prevend < currend[i]) curr = currend[i]-curradd;
-                curradd = curradd + (skill[i]*mana[j]); 
+        int n = skill.size(), m = mana.size();
+        vector<ll> times(n);
+        for (int j = 0; j < m; j++) {
+            ll cur_time = 0;
+            for (int i = 0; i < n; i++) {
+                cur_time = max(cur_time, times[i]) + skill[i] * mana[j];
             }
-            curradd = 0;
-            for(int i=0; i<n; i++)
-            {
-                curradd = curradd + (skill[i]*mana[j]);
-                currend[i] = curr + curradd;
+            times[n - 1] = cur_time;
+            for (int i = n - 2; i >= 0; i--) {
+                times[i] = times[i + 1] - skill[i + 1] * mana[j];
             }
-            st = currend[n-1];
         }
-        return st;
+        return times[n - 1];
     }
 };
