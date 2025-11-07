82cddaaff1c3a68edfa13051fc27b2075b176786
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2025-11-07 23:49:14 +0530

    Time: 260 ms (10.09%), Space: 342.7 MB (5.04%) - LeetHub

--- 2528-maximize-the-minimum-powered-city/2528-maximize-the-minimum-powered-city.cpp
+++ 2528-maximize-the-minimum-powered-city/2528-maximize-the-minimum-powered-city.cpp
@@ -1,48 +1,70 @@
 class Solution {
 public:
     long long maxPower(vector<int>& stations, int r, int k) {
-        int n = stations.size();
-        vector<long long> cnt(n + 1);
-        for (int i = 0; i < n; i++) {
-            int left = max(0, i - r);
-            int right = min(n, i + r + 1);
-            cnt[left] += stations[i];
-            cnt[right] -= stations[i];
+        long long int n = stations.size();
+        vector<long long int> currpow(n);
+        long long int sumpow = 0;
+        for(int i=0; i<n; i++) currpow[i] = stations[i];
+        for(int i=0; i<n; i++)
+        {
+            currpow[i] = currpow[i] + sumpow;
+            sumpow = sumpow + stations[i];
+            if(i-r >= 0) sumpow = sumpow - stations[i-r];
         }
-
-        auto check = [&](long long val) -> bool {
-            vector<long long> diff = cnt;
-            long long sum = 0;
-            long long remaining = k;
-
-            for (int i = 0; i < n; i++) {
-                sum += diff[i];
-                if (sum < val) {
-                    long long add = val - sum;
-                    if (remaining < add) {
-                        return false;
+        sumpow = 0;
+        for(int i=n-1; i>=0; i--)
+        {
+            currpow[i] = currpow[i] + sumpow;
+            sumpow = sumpow + stations[i];
+            if(i+r <= n-1) sumpow = sumpow - stations[i+r];
+        }
+        //for(int i=0; i<n; i++) cout << currpow[i] << " "; cout << endl;
+        long long int st = 0, end = 1e15, mid = st + ((end-st)/2);
+        long long int maxx = 0;
+        while(st <= end)
+        {
+            mid = st + ((end-st)/2);
+            long long int avl = k;
+            long long int relevant = 0;
+            bool check = true;
+            queue<pair<long long int, long long int>> ind;
+            //cout << mid << endl;
+            vector<long long int> pows(n);
+            vector<long long int> rel(n);
+            for(int i=0; i<n; i++)
+            {
+                //long long int temp = currpow[i];
+                rel[i] = relevant;
+                if(currpow[i]+relevant < mid)
+                {
+                    if(mid-(currpow[i]+relevant) > avl)
+                    {
+                        check = false;
+                        break;
                     }
-                    remaining -= add;
-                    int end = min(n, i + 2 * r + 1);
-                    diff[end] -= add;
-                    sum += add;
+                    else
+                    {
+                        avl = avl-(mid-(currpow[i]+relevant));
+                        ind.push({i+(2*r), (mid-(currpow[i]+relevant))});
+                        relevant = relevant + mid-(currpow[i]+relevant);
+                        pows[i] = mid;
+                    }
+                }
+                else pows[i] = currpow[i];
+                if(!ind.empty())
+                {
+                    pair<long long int, long long int> temp = ind.front();
+                    if(temp.first == i)
+                    {
+                        ind.pop();
+                        relevant = relevant - temp.second;
+                    }
                 }
             }
-            return true;
-        };
-
-        long long lo = ranges::min(stations);
-        long long hi = accumulate(stations.begin(), stations.end(), 0LL) + k;
-        long long res = 0;
-        while (lo <= hi) {
-            long long mid = lo + (hi - lo) / 2;
-            if (check(mid)) {
-                res = mid;
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
-            }
+            //if(mid == 118) { for(int i=0; i<n; i++) cout << rel[i] << " " ; cout << endl;}
+            if(check) {maxx = max(maxx, mid); st = mid + 1;}
+            else end = mid-1;
         }
-        return res;
+        return maxx;
     }
 };
