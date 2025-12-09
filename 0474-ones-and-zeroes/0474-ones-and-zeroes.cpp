7ccf5311f6807e22addd0d55b93f4aa42d1c6b77
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2025-12-09 17:39:26 +0530

    Time: 243 ms (8.88%), Space: 175.8 MB (5.03%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
-    int dp[601][101][101];
-
-    int solve(int i, int used0, int used1, const vector<pair<int,int>>& cost, int m, int n) {
-        if (used0 > m || used1 > n) return -1e9;
-        if (i == cost.size()) return 0;
-
-        if (dp[i][used0][used1] != -1)
-            return dp[i][used0][used1];
-
-        int take = 1 + solve(i + 1, used0 + cost[i].first, used1 + cost[i].second, cost, m, n);
-        int skip = solve(i + 1, used0, used1, cost, m, n);
-
-        return dp[i][used0][used1] = max(take, skip);
-    }
-
+    // dp[i][j][k] = in an array of length - max elements i could choose with at max j 0s and k 1s;
+    // dp[i][j][k] = max(dp[i-1][j][k], dp[i-1][j-(no of 0s)][k-(no of 1s)]+1);
     int findMaxForm(vector<string>& strs, int m, int n) {
-        memset(dp, -1, sizeof(dp));
-
-        vector<pair<int,int>> cost;
-        for (auto& s : strs) {
-            int zero = 0, one = 0;
-            for (char c : s) (c == '0' ? zero : one)++;
-            cost.push_back({zero, one});
+        long long int sz = strs.size();
+        vector<pair<long long int, long long int>> bits(sz+1, {0,0});
+        vector<vector<vector<long long int>>> dp(sz+1, vector<vector<long long int>>(m+1, vector<long long int>(n+1, 0)));
+        for(int i=1; i<=sz; i++)
+        {
+            long long int cnt = 0;
+            for(int j=0; j<strs[i-1].size(); j++) if(strs[i-1][j] == '1') cnt = cnt + 1;
+            bits[i].first = strs[i-1].size()-cnt; 
+            bits[i].second = cnt;
         }
-
-        return solve(0, 0, 0, cost, m, n);
+        for(int i=1; i<=sz; i++)
+        {
+            for(int j=0; j<=m; j++)
+            {
+                if(bits[i].first <= j)
+                {
+                    for(int k=0; k<=n; k++)
+                    {
+                        if(bits[i].second <= k)
+                        {
+                            dp[i][j][k] = max(dp[i-1][j][k], dp[i-1][j-bits[i].first][k-bits[i].second]+1);
+                        }
+                        else dp[i][j][k] = dp[i-1][j][k];
+                    }
+                }
+                else for(int k=0; k<=n; k++) dp[i][j][k] = dp[i-1][j][k];
+            }
+        }
+        //for(int i=1; i<=sz; i++) cout << dp[i][m][n] << " "; cout << endl;
+        //if(bits[1].first == 3) cout << dp[2][3][5] << " " << dp[1][3][5] << endl;
+        return dp[sz][m][n];
     }
 };
