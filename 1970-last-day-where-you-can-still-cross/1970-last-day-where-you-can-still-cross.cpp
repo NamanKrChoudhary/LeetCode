ec9bd7d584dae926bdff2beffcd0ccd10ba010f5
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2026-01-02 20:24:47 +0530

    Time: 485 ms (8.18%), Space: 191 MB (47.24%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
@@ -1,70 +1,55 @@
-class DSU {
-    vector<int> root;
-    vector<int> size;
-
-public:
-    DSU(int n) : root(n), size(n, 1) { iota(root.begin(), root.end(), 0); }
-
-    int find(int x) {
-        if (root[x] != x)
-            root[x] = find(root[x]);
-
-        return root[x];
-    }
-
-    void unite(int x, int y) {
-        int rx = find(x);
-        int ry = find(y);
-
-        if (rx == ry)
-            return;
-
-        if (size[rx] > size[ry]) {
-            int t = rx;
-            rx = ry;
-            ry = t;
-        }
-
-        root[rx] = ry;
-        size[ry] += size[rx];
-    }
-};
-
 class Solution {
 public:
     int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
-        DSU dsu(row * col + 2);
-        vector<vector<int>> grid(row, vector<int>(col, 0));
-        int dirs[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
-        for (int i = (int)cells.size() - 1; i >= 0; i--) {
-            int r = cells[i][0] - 1;
-            int c = cells[i][1] - 1;
-            grid[r][c] = 1;
-
-            int id1 = r * col + c + 1;
-
-            for (int k = 0; k < 4; k++) {
-                int nr = r + dirs[k][0];
-                int nc = c + dirs[k][1];
-
-                if (nr >= 0 && nr < row && nc >= 0 && nc < col &&
-                    grid[nr][nc] == 1) {
-                    int id2 = nr * col + nc + 1;
-                    dsu.unite(id1, id2);
+        map<pair<long long int, long long int>, long long int> lastavl;
+        for(int i=0; i<cells.size(); i++) lastavl[{cells[i][0]-1, cells[i][1]-1}] = i;
+        priority_queue<vector<long long int>> q;
+        vector<vector<long long int>> arr(row, vector<long long int>(col, 0));
+        for(int i=0; i<col; i++) {arr[0][i] = lastavl[{0, i}]; q.push({arr[0][i], 0, i});}
+        while(!q.empty())
+        {
+            vector<long long int> curr = q.top();
+            q.pop();
+            long long int curri = curr[1], currj = curr[2];
+            if(curri < row-1)
+            {
+                long long int temp = min(curr[0], lastavl[{curri+1, currj}]);
+                if(temp > arr[curri+1][currj])
+                {
+                    arr[curri+1][currj] = temp;
+                    q.push({temp, curri+1, currj});
                 }
             }
-
-            if (r == 0)
-                dsu.unite(0, id1);
-
-            if (r == row - 1)
-                dsu.unite(row * col + 1, id1);
-
-            if (dsu.find(0) == dsu.find(row * col + 1))
-                return i;
+            if(curri > 0)
+            {
+                long long int temp = min(curr[0], lastavl[{curri-1, currj}]);
+                if(temp > arr[curri-1][currj])
+                {
+                    arr[curri-1][currj] = temp;
+                    q.push({temp, curri-1, currj});
+                }
+            }
+            if(currj < col-1)
+            {
+                long long int temp = min(curr[0], lastavl[{curri, currj+1}]);
+                if(temp > arr[curri][currj+1])
+                {
+                    arr[curri][currj+1] = temp;
+                    q.push({temp, curri, currj+1});
+                }
+            }
+            if(currj > 0)
+            {
+                long long int temp = min(curr[0], lastavl[{curri, currj-1}]);
+                if(temp > arr[curri][currj-1])
+                {
+                    arr[curri][currj-1] = temp;
+                    q.push({temp, curri, currj-1});
+                }
+            }
         }
-
-        return -1;
+        long long int req = 0;
+        for(int i=0; i<col; i++) req = max(req, arr[row-1][i]);
+        return req;
     }
 };
