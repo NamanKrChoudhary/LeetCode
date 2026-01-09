c0886f9f266eab48ac2c4aa89317768bea7cd161
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2026-01-10 02:15:55 +0530

    Time: 0 ms (100%), Space: 17.2 MB (6%) - LeetHub

--- 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -1,22 +1,58 @@
-// this version does not use helper function
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int maxDepth=0;
-    TreeNode* subtreeWithAllDeepest(TreeNode* root, int* depth=NULL, int level=0) {
-        int d0=0;
-        if (!depth) depth=&d0;
-
-        if (!root) {
-            *depth=level;
-            maxDepth=max(maxDepth, *depth);
-            return root;
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        map<TreeNode* , long long int> nodelvl;
+        map<TreeNode*, TreeNode*> parent;
+        queue<TreeNode*> q;
+        q.push(root);
+        long long int cnt = 0;
+        set<TreeNode*> lowest;
+        while(!q.empty())
+        {
+            cnt = cnt + 1;
+            long long int len = q.size();
+            set<TreeNode*> temp;
+            for(int i=0; i<len; i++)
+            {
+                TreeNode* currnode = q.front();
+                temp.insert(q.front());
+                q.pop();
+                nodelvl[currnode] = cnt;
+                if(currnode->left != NULL) 
+                {
+                    q.push(currnode->left);
+                    parent[currnode->left] = currnode;
+                }
+                if(currnode->right != NULL) 
+                {
+                    q.push(currnode->right);
+                    parent[currnode->right] = currnode;
+                }
+            }
+            if(q.empty()) lowest = temp;
         }
-        int lD=0, rD=0;
-        TreeNode* L=subtreeWithAllDeepest(root->left, &lD, level+1);
-        TreeNode* R=subtreeWithAllDeepest(root->right, &rD, level+1);
-
-        *depth=max(lD, rD);
-        if (lD==rD && lD==maxDepth) return root;
-        return (lD>rD)?L:R;
+        //cout << lowest.size() << endl;
+        while(lowest.size() > 1)
+        {
+            set<TreeNode*> next;
+            for(auto i: lowest)
+            {
+                next.insert(parent[i]);
+                //cout << i->val << " " << parent[i]->val << endl;
+            }
+            lowest = next;
+        }
+        return *lowest.begin();
     }
 };
