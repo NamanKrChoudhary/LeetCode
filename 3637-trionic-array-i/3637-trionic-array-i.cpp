df2cd5243c46353f67fa5d2aa6423cb4c2ee71d9
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2026-02-04 12:36:59 +0530

    Time: 0 ms (100%), Space: 27.8 MB (35.37%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.cpp
+++ 3637-trionic-array-i/3637-trionic-array-i.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool isTrionic(vector<int>& nums) {
-        int n = nums.size(), i = 1;
-        while (i < n && nums[i - 1] < nums[i]) {
-            i++;
+        long long int n = nums.size();
+        long long int prev;
+        if(nums[1] > nums[0]) prev = 1;
+        else return false;
+        vector<long long int> arr;
+        arr.push_back(prev);
+        for(int i=2; i<n; i++)
+        {
+            if(nums[i] == nums[i-1]) return false;
+            if(nums[i] > nums[i-1] && prev == 2) {prev = 1; arr.push_back(prev);}
+            else if(nums[i] < nums[i-1] && prev == 1) {prev = 2; arr.push_back(prev);}
         }
-        int p = i - 1;
-        while (i < n && nums[i - 1] > nums[i]) {
-            i++;
-        }
-        int q = i - 1;
-        while (i < n && nums[i - 1] < nums[i]) {
-            i++;
-        }
-        int flag = i - 1;
-        return (p != 0) && (q != p) && (flag == n - 1 && flag != q);
+        if(arr.size() != 3) return false;
+        else return true;
     }
 };
