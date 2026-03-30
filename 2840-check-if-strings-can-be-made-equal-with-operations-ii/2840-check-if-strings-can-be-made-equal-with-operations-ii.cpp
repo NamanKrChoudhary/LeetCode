c78081d51c5b68e16d3ec1414a9f56a0f4f1fbde
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2026-03-30 11:13:53 +0530

    Time: 56 ms (24.14%), Space: 26.1 MB (5.75%) - LeetHub

---
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        long long int n = s1.size();
+        vector<char> odd1, odd2, eve1, eve2;
+        for(int i=0; i<n; i++)
+        {
+            if(i%2 == 0) 
+            {
+                eve1.push_back(s1[i]);
+                eve2.push_back(s2[i]);
+            }
+            else
+            {
+                odd1.push_back(s1[i]);
+                odd2.push_back(s2[i]);
+            }
+        }
+        sort(eve1.begin(), eve1.end());
+        sort(odd1.begin(), odd1.end());
+        sort(eve2.begin(), eve2.end());
+        sort(odd2.begin(), odd2.end());
+        if(odd1 == odd2 && eve1 == eve2) return true;
+        else return false;
+    }
+};
