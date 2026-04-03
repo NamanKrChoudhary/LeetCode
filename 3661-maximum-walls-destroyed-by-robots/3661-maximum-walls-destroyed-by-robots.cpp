7fdbff519681fedd9273e48d6876ac1e9e2bd5c2
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2026-04-04 04:26:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3661-maximum-walls-destroyed-by-robots/3661-maximum-walls-destroyed-by-robots.cpp
+++ 3661-maximum-walls-destroyed-by-robots/3661-maximum-walls-destroyed-by-robots.cpp
@@ -1,90 +1,133 @@
 class Solution {
 public:
-    int maxWalls(vector<int>& robots, vector<int>& distance,
-                 vector<int>& walls) {
-        int n = robots.size();
-        vector<pair<int, int>> robotDist;
-        for (int i = 0; i < n; i++) {
-            robotDist.push_back({robots[i], distance[i]});
+    int maxWalls(vector<int>& robots, vector<int>& distance, vector<int>& walls) {
+        long long int n = robots.size();
+        vector<vector<long long int>> arr;
+        for(int i=0; i<n; i++)
+        {
+            arr.push_back({robots[i], 1, distance[i]});
         }
-        sort(robotDist.begin(), robotDist.end());
-        sort(walls.begin(), walls.end());
-
-        int m = walls.size();
-        int rightPtr = 0, leftPtr = 0, curPtr = 0, robotPtr = 0;
-
-        // DP variable: saves only the previous value
-        int prevLeft = 0, prevRight = 0, prevNum = 0;
-        int subLeft = 0, subRight = 0;
-
-        for (int i = 0; i < n; i++) {
-            int robotPos = robotDist[i].first;
-            int robotDistVal = robotDist[i].second;
-
-            // rightPtr finds the first position greater than robotPos
-            // (corresponding to upper_bound)
-            while (rightPtr < m && walls[rightPtr] <= robotPos) {
-                rightPtr++;
-            }
-            int pos1 = rightPtr;
-
-            // curPtr finds the first position >= robotPos (corresponding to
-            // lower_bound)
-            while (curPtr < m && walls[curPtr] < robotPos) {
-                curPtr++;
-            }
-            int pos2 = curPtr;
-
-            // leftPtr finds the first wall >= the left boundary
-            int leftBound = (i >= 1) ? max(robotPos - robotDistVal,
-                                           robotDist[i - 1].first + 1)
-                                     : robotPos - robotDistVal;
-            while (leftPtr < m && walls[leftPtr] < leftBound) {
-                leftPtr++;
-            }
-            int leftPos = leftPtr;
-            int currentLeft = pos1 - leftPos;
-
-            // Count the number of walls that can be reached on the right side
-            int rightBound = (i < n - 1) ? min(robotPos + robotDistVal,
-                                               robotDist[i + 1].first - 1)
-                                         : robotPos + robotDistVal;
-            while (rightPtr < m && walls[rightPtr] <= rightBound) {
-                rightPtr++;
-            }
-            int rightPos = rightPtr;
-            int currentRight = rightPos - pos2;
-
-            // robotPtr finds the position of the previous robot
-            int currentNum = 0;
-            if (i > 0) {
-                while (robotPtr < m &&
-                       walls[robotPtr] < robotDist[i - 1].first) {
-                    robotPtr++;
+        set<long long int> robopos;
+        for(int i=0; i<n; i++) robopos.insert(robots[i]);
+        long long int extra = 0;
+        for(int i=0; i<walls.size(); i++)
+        {
+            if(robopos.find(walls[i]) != robopos.end()) extra = extra + 1;
+            else arr.push_back({walls[i], 2});
+        }
+        sort(arr.begin(), arr.end());
+        long long int wallnos = 0;
+        long long int prevpos = -1;
+        long long int distcov = -1;
+        map<long long int, long long int> right;
+        for(int i=0; i<arr.size(); i++)
+        {
+            if(prevpos == -1)
+            {
+                if(arr[i][1] == 2) continue;
+                else 
+                {
+                    prevpos = arr[i][0];
+                    distcov = arr[i][0] + arr[i][2];
                 }
-                int pos3 = robotPtr;
-                currentNum = pos1 - pos3;
             }
-
-            if (i == 0) {
-                subLeft = currentLeft;
-                subRight = currentRight;
-            } else {
-                int newsubLeft =
-                    max(subLeft + currentLeft,
-                        subRight - prevRight +
-                            min(currentLeft + prevRight, currentNum));
-                int newsubRight =
-                    max(subLeft + currentRight, subRight + currentRight);
-                subLeft = newsubLeft;
-                subRight = newsubRight;
+            else
+            {
+                if(arr[i][1] == 2)
+                {
+                    if(arr[i][0] > distcov)
+                    {
+                        right[prevpos] = wallnos;
+                        prevpos = -1;
+                        distcov = -1;
+                        wallnos = 0;
+                    }
+                    else wallnos = wallnos + 1;
+                }
+                else
+                {
+                    right[prevpos] = wallnos;
+                    prevpos = arr[i][0];
+                    distcov = arr[i][0]+arr[i][2];
+                    wallnos = 0;
+                }
             }
-
-            prevLeft = currentLeft;
-            prevRight = currentRight;
-            prevNum = currentNum;
         }
-
-        return max(subLeft, subRight);
+        if(prevpos != -1)
+        {
+            right[prevpos] = wallnos;
+            wallnos = 0;
+            prevpos = -1;
+            distcov = -1;
+        }
+        map<long long int, long long int> left;
+        for(int i=arr.size()-1; i>=0; i--)
+        {
+            if(prevpos == -1)
+            {
+                if(arr[i][1] == 2) continue;
+                else 
+                {
+                    prevpos = arr[i][0];
+                    distcov = arr[i][0] - arr[i][2];
+                }
+            }
+            else
+            {
+                if(arr[i][1] == 2)
+                {
+                    if(arr[i][0] < distcov)
+                    {
+                        right[prevpos] = wallnos;
+                        prevpos = -1;
+                        distcov = -1;
+                        wallnos = 0;
+                    }
+                    else wallnos = wallnos + 1;
+                }
+                else
+                {
+                    right[prevpos] = wallnos;
+                    prevpos = arr[i][0];
+                    distcov = arr[i][0]+arr[i][2];
+                    wallnos = 0;
+                }
+            }
+        }
+        if(prevpos != -1)
+        {
+            right[prevpos] = wallnos;
+            wallnos = 0;
+            prevpos = -1;
+            distcov = -1;
+        }
+        vector<long long int> roboind;
+        for(int i=0; i<arr.size(); i++) if(arr[i][1] == 1) roboind.push_back(i);
+        map<pair<long long int, long long int>, long long int> mid;
+        for(int i=1; i<roboind.size(); i++)
+        {
+            long long int wallno = 0;
+            for(int j=roboind[i-1]+1; j<roboind[i]; j++) wallno = wallno + 1;
+            mid[{i-1, i}] = wallno; 
+        }
+        vector<vector<long long int>> dp(n, vector<long long int>(2));
+        dp[0][0] = left[arr[roboind[0]][0]];
+        dp[0][1] = right[arr[roboind[0]][0]];
+        for(int i=1; i<n; i++)
+        {
+            dp[i][1] = max(dp[i-1][0]+right[arr[roboind[i]][0]], dp[i-1][1]+right[arr[roboind[i]][0]]);
+            if(arr[roboind[i]][0]-arr[roboind[i-1]][0] <= arr[roboind[i]][2]+arr[roboind[i-1]][2])
+            {
+                dp[i][0] = max(dp[i-1][0]+ left[arr[roboind[i]][0]], dp[i-1][1]-right[arr[roboind[i-1]][0]]+mid[{i-1, i}]);
+                //cout << right[arr[roboind[i]][0]] << " " << mid[{i-1, i}] << endl;
+            }
+            else
+            {
+                dp[i][0] = max(dp[i-1][0]+left[arr[roboind[i]][0]], dp[i-1][1]+left[arr[roboind[i]][0]]);
+            }
+        }
+        //for(int i=0; i<n; i++) cout << dp[i][0] << " " << dp[i][1] << endl;
+        //cout << extra << endl;
+        return max(dp[n-1][0], dp[n-1][1])+extra;
     }
 };
