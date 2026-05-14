9c02692a0039c07957aaaacd80ed7f77ff429dca
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2026-05-14 16:51:59 +0530

    Time: 125 ms (12.18%), Space: 127.4 MB (10.35%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -1,13 +1,32 @@
 class Solution {
 public:
     int minimumEffort(vector<vector<int>>& tasks) {
-        sort(tasks.begin(), tasks.end(), [&](vector<int>& a, vector<int>& b) {
-            return a[1] - a[0] < b[1] - b[0];
-        });
-        int ans = 0;
-        for (auto task : tasks) {
-            ans = max(ans + task[0], task[1]);
+        long long int minn = 0;
+        long long int n = tasks.size();
+        for(int i=0; i<n; i++) minn = minn + tasks[i][0];
+        vector<vector<long long int>> arr(n, vector<long long int>(3));
+        for(int i=0; i<n; i++)
+        {
+            arr[i][0] = tasks[i][1] - tasks[i][0];
+            arr[i][1] = tasks[i][1];
+            arr[i][2] = tasks[i][0];
         }
-        return ans;
+        sort(arr.rbegin(), arr.rend());
+        long long int extra = 0;
+        long long int temp = minn;
+        //for(int i=0; i<n; i++) cout << arr[i][0] << " " << arr[i][1] << endl;
+        for(int i=0; i<n; i++)
+        {
+            //cout << minn << endl;
+            if(minn >= arr[i][1]) {minn = minn-arr[i][2]; continue;}
+            else
+            {
+                extra = extra + (arr[i][1]-minn);
+                minn = arr[i][1];
+                minn = minn - arr[i][2];
+            }
+        }
+        //cout << temp << " " << extra << endl;
+        return temp + extra;
     }
 };
