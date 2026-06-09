c952b4cad23b9724f52106751903d5914ca8b5d7
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2026-06-09 17:53:53 +0530

    Time: 4 ms (33.07%), Space: 103.7 MB (69.26%) - LeetHub

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     long long maxTotalValue(vector<int>& nums, int k) {
-        long long int n = nums.size();
-        multiset<long long int> minn;
-        multiset<long long int> maxx;
-        for(int i=0; i<n; i++)
+        int maxx = 0, minn = 1e9+7;
+        for(int i=0; i<nums.size(); i++)
         {
-            minn.insert(nums[i]);
-            maxx.insert(nums[i]);
+            maxx = max(maxx, nums[i]);
+            minn = min(minn, nums[i]);
         }
-        long long int val = ((*maxx.rbegin())) - (*minn.begin());
-        // for(int i=k, j=0; i<n; i++,j++)
-        // {
-        //     auto it1 = maxx.find(nums[j]);
-        //     maxx.erase(it1);
-        //     auto it2 = minn.find(nums[j]);
-        //     minn.erase(it2);
-        //     maxx.insert(nums[i]);
-        //     minn.insert(nums[i]);
-        //     long long int temp = (*maxx.rbegin()) - (*minn.begin());
-        //     val = max(val, temp);
-        // }
-        return k*val;
+        long long int min1 = minn, max1 = maxx;
+        long long int req = (max1-min1)*k;
+        return req;
     }
 };
