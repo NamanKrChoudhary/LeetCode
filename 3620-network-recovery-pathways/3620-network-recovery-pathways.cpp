14d5ee2207b6388a0c5b242f32b576e7d79280f2
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2026-07-05 00:42:20 +0530

    Time: 589 ms (27.13%), Space: 400.7 MB (26.36%) - LeetHub

--- 3620-network-recovery-pathways/3620-network-recovery-pathways.cpp
+++ 3620-network-recovery-pathways/3620-network-recovery-pathways.cpp
@@ -7,18 +7,14 @@ public:
         for(int i=0; i<edges.size(); i++) adj[edges[i][0]].push_back({edges[i][2], edges[i][1]});
         long long int st = 0, end = 1e9;
         long long int req = -1;
-        //long long int cnt = 0;
         while(st <= end)
         {
             long long int mid = st + ((end-st)/2);
-            //vector<long long int> vis(n, 1e15);
-            vector<bool> vis(n, false);
+            vector<long long int> vis(n, 1e15);
             priority_queue<pair<long long int, long long int>, vector<pair<long long int, long long int>>, greater<pair<long long int, long long int>>> pq;
             pq.push({0,0});
-            //vis[0] = true;
+            vis[0] = 0;
             bool possible = false;
-            //cout << cnt << endl;
-            //cout << mid << endl;
             while(!pq.empty())
             {
                 pair<long long int, long long int> curr = pq.top();
@@ -26,18 +22,15 @@ public:
                 long long int currcost = curr.first;
                 long long int currnode = curr.second;
                 if(currnode == n-1) possible = true;
-                if(vis[currnode] == true) continue;
-                vis[currnode] = true;
+                if(vis[currnode] < currcost) continue;
                 for(int i=0; i<adj[currnode].size(); i++)
                 {
-                    //cnt++;
                     long long int nextnode = adj[currnode][i].second;
                     long long int nodecost = adj[currnode][i].first;
                     long long int nextcost = currcost + nodecost;
-                    //cout << nextnode << " " << nextcost << endl;
-                    if(vis[nextnode] == false && nextcost <= k && online[nextnode] == true && nodecost >= mid)
+                    if(vis[nextnode] > nextcost && nextcost <= k && online[nextnode] == true && nodecost >= mid)
                     {
-                        //vis[nextnode] = nextcost;
+                        vis[nextnode] = nextcost;
                         pq.push({nextcost, nextnode});
                     }
                 }
