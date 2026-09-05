4fd8eabbf434b40d3913f7c4a4f7ab3100bf4c4c
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2026-09-05 16:21:58 +0530

    Time: 19 ms (25.11%), Space: 210.7 MB (56.49%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        long long int n = nums.size();
+        vector<int> premax(n,0), sufmin(n,0); premax[0] = nums[0];
+        sufmin[n-1] = nums[n-1];
+        for(int i=1; i<n; i++)
+        {
+            premax[i] = max(premax[i-1], nums[i]);
+        }
+        for(int i=n-2; i>=0; i--) sufmin[i] = min(sufmin[i+1], nums[i]);
+        for(int i=0; i<n; i++)
+        {
+            if(premax[i]-sufmin[i] <= k) return i;
+        }
+        return -1;
+    }
+};
