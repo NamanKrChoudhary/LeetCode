87c9be3084f5ebbcd1cf4658a1f7b9cd3b7d883c
NamanKrChoudhary <168548097+NamanKrChoudhary@users.noreply.github.com>
2026-09-06 15:16:08 +0530

    Time: 27 ms (71.17%), Space: 44.1 MB (38.48%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        vector<vector<int>> v(t.size(), vector<int>(s.size()));
-        for(int i=0; i<t.size(); i++)
+        long long int n = t.size(), m = s.size();
+        vector<vector<unsigned long long int>> dp(n, vector<unsigned long long int>(m, 0));
+        if(s[m-1] == t[n-1]) dp[n-1][m-1] = 1;
+        for(int i=n-1; i>=0; i--)
         {
-            long long int prev = 0;
-            for(int j=i; j<s.size()-(t.size()-i-1); j++)
+            for(int j=m-1; j>=0; j--)
             {
-                if(s[j] == t[i])
+                if(i == n-1 && j == m-1) continue;
+                if(t[i] == s[j])
                 {
-                    if(i>0 && j>0) v[i][j] = prev + v[i-1][j-1];
-                    else v[i][j] = 1+prev;
-                    prev = v[i][j];
+                    if(i < n-1 && j < m-1) dp[i][j] += dp[i+1][j+1] + dp[i][j+1];
+                    else if(j < m-1) dp[i][j] += 1 + dp[i][j+1];
                 }
                 else
                 {
-                    v[i][j] = prev;
+                    if(j < m-1) dp[i][j] += dp[i][j+1];
                 }
             }
         }
-        return v[t.size()-1][s.size()-1];
+        // for(int i=0; i<n; i++)
+        // {
+        //     for(int j=0; j<m; j++) cout << dp[i][j] << " "; cout << endl;
+        // }
+        return dp[0][0];
     }
 };
